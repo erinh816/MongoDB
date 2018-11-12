8a467019c4909075fac6f4709292dbb995e50fdc
Silu Hu <silu@Silus-MacBook-Pro.local>
2018-11-12 17:23:15 -0500

    add what Anuj added

--- App.js
+++ App.js
@@ -1,42 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, NavigatorIOS, Text, View } from 'react-native';
 import {
-  StyleSheet,
-  Text,
-  View,
-  ImageBackground,
-  Image,
-  TouchableOpacity,
-  Button
-} from 'react-native';
+  createStackNavigator,
+} from 'react-navigation';
+import CameraView from './components/CameraView';
+import HomeScreen from './components/HomeScreen';
 
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <ImageBackground scource={require('./assets/eye.png')}
-        style={styles.container}>
-        <Text style={styles.text}>ClarifEYE</Text>
-        <Image style={styles.image} source={require('./eye.png')} />
-        <Button title='BUTTON' />
-      </ImageBackground>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#F2BB05',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 40,
-  },
-  image: {
-    width: 200,
-    height: 200
-  }
+const App = createStackNavigator({
+  Home: { screen: HomeScreen },
+  CameraView: { screen: CameraView },
 });
 
-export default;
+export default App;
